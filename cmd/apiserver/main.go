40a7530b6f9975d391a41003504e835ca9480bfc
Taekun <gtexan@gmail.com>
2020-10-15 21:30:05 +0500

    Обновление _12_03

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"log"
+	"flag"
 	"github.com/taekun/apiserver/internal/app/apiserver"
 )
 
+var(
+	configpath string
+)
+
+func init(){
+	flag.StringVar(&configpath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
 func main()
 {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configpath, config)
+	if err !=nil{
+		log.Fatal(err)
+	}
+
 	s := apiserver.New()
 	if err := s.Start(); err != nil{
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/taekun/ApiServer
 
 go 1.14
+
+require github.com/BurntSushi/toml v0.3.1 // indirect
